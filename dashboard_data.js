c5ce8359c4f8526f7344a8f4c0354d24562722f6
pierrep180 <71437764+pierrep180@users.noreply.github.com>
2026-07-20 09:59:22 +0200

    Add files via upload

--- dashboard_data.js
+++ dashboard_data.js
@@ -5,7 +5,7 @@
    Ziele (goals, followerGoal, goals2026) werden dabei bewahrt.
    ============================================================ */
 window.SW_DATA = {
-  updated: "2026-07-17",
+  updated: "2026-07-20",
   monthLabel: "Juli 2026",
 
   goals: { followersTotal: 200000, viewsMonthly: 10000000 },
@@ -21,26 +21,26 @@ window.SW_DATA = {
     },
     {
       key:"leipzig", name:"Leipzig",
-      followers:25495, _monthStartFollowers:22600, followerGoal:35000,
-      viewsMonthly:895811, newThisMonth:2895, deltaViewsPct:-5.8,
-      erreichteKonten:151393, interaktionen:37489,
-      newestPost:{typ:"p",code:"Da2o6xMArjU",likes:156,comments:0,shares:1},
+      followers:25596, _monthStartFollowers:22600, followerGoal:35000,
+      viewsMonthly:909272, newThisMonth:2996, deltaViewsPct:-4.3,
+      erreichteKonten:154650, interaktionen:38134,
+      newestPost:{typ:"reel",code:"Da_F3YEii-D",likes:110,comments:1,shares:4},
       goals2026:["35.000 Follower","Mehr Ausflüge, Unternehmungen & Gratis-Tipps","Dauerhaft passende Kooperationspartner"]
     },
     {
       key:"chemnitz", name:"Chemnitz",
-      followers:22786, _monthStartFollowers:21100, followerGoal:30000,
-      viewsMonthly:1188884, newThisMonth:1686, deltaViewsPct:23.3,
-      erreichteKonten:77778, interaktionen:37432,
-      newestPost:{typ:"p",code:"Da20QEKDLrC",likes:213,comments:0,shares:0},
+      followers:22829, _monthStartFollowers:21100, followerGoal:30000,
+      viewsMonthly:1187295, newThisMonth:1729, deltaViewsPct:23.2,
+      erreichteKonten:78127, interaktionen:37636,
+      newestPost:{typ:"p",code:"Da-vu-wDPzB",likes:146,comments:0,shares:1},
       goals2026:["30.000 Follower","Team ausbauen","Content-Vielfalt & neue Gesichter etablieren"]
     },
     {
       key:"erfurt", name:"Erfurt",
-      followers:17283, _monthStartFollowers:15774, followerGoal:25000,
-      viewsMonthly:1108329, newThisMonth:1509, deltaViewsPct:-9.6,
-      erreichteKonten:168465, interaktionen:55311,
-      newestPost:{typ:"p",code:"Da2KZrICEiz",likes:177,comments:0,shares:0},
+      followers:17374, _monthStartFollowers:15774, followerGoal:25000,
+      viewsMonthly:1090807, newThisMonth:1600, deltaViewsPct:-11.1,
+      erreichteKonten:167885, interaktionen:54380,
+      newestPost:{typ:"reel",code:"Da-ozISIS8Q",likes:360,comments:5,shares:2},
       goals2026:["25.000 Follower","Zur Go-to-Seite der Stadt werden","Erste passende Kooperationspartner"]
     }
   ]
